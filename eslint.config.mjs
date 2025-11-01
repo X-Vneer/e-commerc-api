2d89a3641e286affb059771be1624f3ce56a8d3d
Moneer <xv.neer.business@gmail.com>
2025-11-01 10:08:57 +0200

    setup prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,38 @@
-import antfu from "@antfu/eslint-config";
+import antfu from "@antfu/eslint-config"
+import prettierConfig from "eslint-config-prettier"
 
-export default antfu({
-  type: "app",
-  typescript: true,
-  formatters: true,
-  stylistic: {
-    indent: 2,
-    semi: false,
-    quotes: "double",
+export default antfu(
+  {
+    type: "app",
+    typescript: true,
+    formatters: {
+      prettier: true,
+    },
   },
-}, {
-  rules: {
-    "ts/no-redeclare": "off",
-    "node/file-extension-in-import": ["error", "always"],
-    "ts/consistent-type-definitions": ["error", "type"],
-    "no-console": ["warn"],
-    "antfu/no-top-level-await": ["off"],
-    "node/prefer-global/process": ["off"],
-    "node/no-process-env": ["error"],
-    "perfectionist/sort-imports": ["error", {
-      tsconfigRootDir: ".",
-    }],
-    "unicorn/filename-case": ["error", {
-      case: "kebabCase",
-      ignore: ["README.md"],
-    }],
-    "test/prefer-lowercase-title": ["off"],
+  {
+    rules: {
+      ...prettierConfig.rules,
+      "ts/no-redeclare": "off",
+      "node/file-extension-in-import": ["error", "always"],
+      "ts/consistent-type-definitions": ["error", "type"],
+      "no-console": ["warn"],
+      "antfu/no-top-level-await": ["off"],
+      "node/prefer-global/process": ["off"],
+      "node/no-process-env": ["error"],
+      "perfectionist/sort-imports": [
+        "error",
+        {
+          tsconfigRootDir: ".",
+        },
+      ],
+      "unicorn/filename-case": [
+        "error",
+        {
+          case: "kebabCase",
+          ignore: ["README.md"],
+        },
+      ],
+      "test/prefer-lowercase-title": ["off"],
+    },
   },
-});
+)
